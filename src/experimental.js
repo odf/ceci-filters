859eb2130b58c1bf4b58974c31b10c26b2acbb7c
odf <olaf.delgado@gmail.com>
2014-01-20 21:10:43 +1100

    further work on experimental version

--- src/experimental.js
+++ src/experimental.js
@@ -9,89 +9,111 @@ const SKIP = 1;
 const STOP = 2;
 
 
-var lift_raw = function(fn) {
-  return function(input, output) {
-    cc.go(function*() {
-      var arg, val, okay;
+var pipeThrough = function(fn, input, output) {
+  var managed = output == null;
+  if (managed)
+    output = cc.chan();
 
-      while (true) {
-        arg = yield cc.pull(input);
-        if (arg === undefined)
-          break;
+  core.go(function*() {
+    var arg, res, val, status;
 
-        [val, status] = fn(arg);
-        if (status == SKIP)
-          continue;
-        else if (status == STOP)
-          break;
+    while (true) {
+      if (undefined === (arg = yield cc.pull(input)))
+        break;
 
-        okay = yield cc.push(output, (val === undefined ? null : val));
-        if(!okay)
-          break;
-      }
-    });
+      res = fn(arg);
+      val = res[0];
+      status = res[1];
+      if (status == SKIP)
+        continue;
+      else if (status == STOP)
+        break;
+
+      if (!(yield cc.push(output, (val === undefined ? null : val))))
+        break;
+    }
+
+    if (managed) {
+      cc.close(input);
+      cc.close(output);
+    }
+  });
+
+  return output;
+};
+
+
+exports.source = function(gen, output) {
+  var managed = output == null;
+  if (managed)
+    output = cc.chan();
+
+  core.go(function*() {
+    var step;
+
+    while (true) {
+      step = gen.next();
+      if (step.done)
+        break;
+      if (!(yield cc.push(output, step.value)))
+        break
+    }
+
+    if (managed)
+      cc.close(output);
+  });
+
+  return output;
+};
+
+
+exports.each = function(fn, input) {
+  return core.go(function*() {
+    var val;
+    while (undefined !== (val = yield cc.pull(input)))
+      fn(val);
   });
 };
 
 
-var lift = function(fn) {
-  var lifted = lift_raw(fn);
-
-  return function(input, output) {
-    if (output == null) {
-      var output = cc.chan();
-
-      cc.go(function*() {
-        yield lifted(input, output);
-        cc.close(input);
-        cc.close(output);
-      });
-
-      return output;
-    } else
-      lifted(input, output);
-  });
+exports.map = function(fn, input, output) {
+  return pipeThrough(
+    function(arg) {
+      return [fn(arg), OKAY];
+    },
+    input, output);
 };
 
 
-var each = function(fn, input) {
-  lift(function(arg) {
-    return [fn(arg), SKIP];
-  })(input);
+exports.filter = function(pred, input, output) {
+  return pipeThrough(
+    function(arg) {
+      return [arg, (pred(arg) ? OKAY : SKIP)];
+    },
+    input, output);
 };
 
 
-var map = function(fn, input, output) {
-  return lift(function(arg) {
-    return [fn(arg), OKAY];
-  })(input, output);
-};
-
-
-var filter = function(pred, input, output) {
-  return lift(function(arg) {
-    return [arg, (pred(arg) ? OKAY : SKIP)];
-  })(input, output);
-};
-
-
-var reductions = function(fn, start, input, output) {
+exports.reductions = function(fn, start, input, output) {
   var acc = start;
-  return lift(function(arg) {
-    return [acc = fn(acc, arg), OKAY];
-  })(input, output);
+  return pipeThrough(
+    function(arg) {
+      return [acc = fn(acc, arg), OKAY];
+    },
+    input, output);
 };
 
 
-var take = function(n, input, output) {
+exports.take = function(n, input, output) {
   if (n == 0) {
     if (output == null)
       return cc.chan();
   } else {
     var count = 0;
-    return lift(function(arg) {
-      ++count;
-      return [arg, (count < n ? OKAY : STOP)];
-    })(input, output);
+    return pipeThrough(
+      function(arg) {
+        return [arg, (++count < n ? OKAY : STOP)];
+      },
+      input, output);
   }
 };
